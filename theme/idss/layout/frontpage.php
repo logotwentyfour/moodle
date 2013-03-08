4799fb5fb1ca0da479b887c9cc71fad7285ab6ba
Patrick O'Hanlon <paddy@paddyohanlon.com>
2013-03-08 12:11:07 +0100

    Do conditional front page.

--- theme/idss/layout/frontpage.php
+++ theme/idss/layout/frontpage.php
@@ -44,18 +44,92 @@ echo $OUTPUT->doctype() ?>
 <div id="custommenu"><?php echo $custommenu; ?></div>
 <?php } ?>
 
-<div class="front-page">
-  <h1 class="headermain">
-    <a href="<?php echo $CFG->wwwroot; ?>" title="Home"><?php echo $PAGE->heading ?></a>
-  </h1>
+<?php if (isloggedin()) { ?>
+    <div id="page">
 
-  <?php # needed or gives error, but not outputting anything ?>
-  <?php echo $OUTPUT->main_content() ?>
+        <div id="wrapper" class="clearfix">
 
-  <p>Returning Dermatology SpRs <a href="login/index.php">Login here</a> 
-    <span>or</span>
-    if you have yet to register on this website, you can <a href="/login/signup.php?">Create a New Account here</a></p>
-</div>
+    <!-- START OF HEADER -->
+
+            <div id="page-header">
+                <div id="page-header-wrapper" class="wrapper clearfix">
+
+                    <div id="header-left">
+                        <h1 class="headermain">
+                          <a href="<?php echo $CFG->wwwroot; ?>" title="Home"><?php echo $PAGE->heading ?></a>
+                        </h1>
+                    </div>
+                    <div class="headermenu">
+                        <?php
+                            echo $OUTPUT->login_info();
+                            echo $OUTPUT->lang_menu();
+                            echo $PAGE->headingmenu;
+                        ?>
+                    </div>
+                </div>
+            </div>
+
+    <!-- END OF HEADER -->
+
+    <!-- START OF CONTENT -->
+
+            <div id="page-content-wrapper" class="wrapper clearfix">
+                <div id="page-content">
+                    <div id="region-main-box">
+                        <div id="region-post-box">
+
+                            <div id="region-main-wrap">
+                                <div id="region-main">
+                                    <div class="region-content">
+                                        <?php echo $OUTPUT->main_content() ?>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <?php if ($hassidepost) { ?>
+                            <div id="region-post" class="block-region">
+                                <div class="region-content">
+                                    <?php echo $OUTPUT->blocks_for_region('side-post') ?>
+                                </div>
+                            </div>
+                            <?php } ?>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+    <!-- END OF CONTENT -->
+
+        </div> <!-- END #wrapper -->
+
+    <!-- START OF FOOTER -->
+        <div id="page-footer" class="wrapper clearfix">
+               <p class="helplink"><?php echo page_doc_link(get_string('moodledocslink')) ?></p>
+            <?php
+                   echo $OUTPUT->login_info();
+                   echo $OUTPUT->home_link();
+                echo $OUTPUT->standard_footer_html();
+               ?>
+           </div>
+
+    <!-- END OF FOOTER -->
+
+    </div> <!-- END #page -->
+<?php } else { ?>
+    <div class="front-page">
+      <h1 class="headermain">
+        <a href="<?php echo $CFG->wwwroot; ?>" title="Home"><?php echo $PAGE->heading ?></a>
+      </h1>
+
+      <?php # needed or gives error, but not outputting anything ?>
+      <?php echo $OUTPUT->main_content() ?>
+
+      <p>Returning Dermatology SpRs <a href="login/index.php">Login here</a> 
+        <span>or</span>
+        if you have yet to register on this website, you can <a href="/login/signup.php?">Create a New Account here</a></p>
+    </div>
+<?php } ?>
 
 <?php echo $OUTPUT->standard_end_of_body_html() ?>
 </body>
